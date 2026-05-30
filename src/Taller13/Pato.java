4bcb1cd7bcfeb1f3e64b8d33aea57d4b83c13661
mrso1701 <andresrodriguez172008@gmail.com>
2026-05-30 15:52:54 -0500

    feat(taller-13): Implementacion de interfaces en clase Pato

--- src/Taller13/Pato.java
+++ src/Taller13/Pato.java
@@ -1,5 +1,13 @@
 package Taller13;
 
-public class Pato {
+public class Pato implements Volador, Nadador {
+    @Override
+    public void volar(){
+        System.out.println("El pato esta volando");
+    }
     
+    @Override
+    public void nadar(){
+        System.out.println("El pato esta nadando");
+    }
 }
