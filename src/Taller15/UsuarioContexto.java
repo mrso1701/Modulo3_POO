c1036e2e72ef70d5184435f28d0ee692d48dcf2c
mrso1701 <andresrodriguez172008@gmail.com>
2026-05-30 17:18:21 -0500

    feature(taller-15): Creacion de clase Contexto para Usuario

---
+++ src/Taller15/UsuarioContexto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller15;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class UsuarioContexto {
+    
+}
