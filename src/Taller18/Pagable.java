1ab91ac457f197887775e9da178076db4ba0e484
mrso1701 <andresrodriguez172008@gmail.com>
2026-06-02 15:34:34 -0500

    feat(taller-18): Division de tareas de interfaces usando IPS

---
+++ src/Taller18/Pagable.java
@@ -0,0 +1,5 @@
+package Taller18;
+
+public interface Pagable {
+    void pagarFactura(String codigoFactura);
+}
---
+++ src/Taller18/Transferible.java
@@ -0,0 +1,5 @@
+package Taller18;
+
+public interface Transferible {
+    void transferir(double monto, String cuentaDestino);
+}
